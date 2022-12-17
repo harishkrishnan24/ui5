0e2daff11977225b8c98d0c5464e30e43f9d488c
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-17 21:19:08 +0530

    turn views into XML views

--- mvc-sample/webapp/controller/Master.controller.js
+++ mvc-sample/webapp/controller/Master.controller.js
@@ -1,5 +1,14 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
 
-  return Controller.extend("sapui5.demo.mvcapp.controller.Master", {});
+  return Controller.extend("sapui5.demo.mvcapp.controller.Master", {
+    onListPress: function (oEvent) {
+      var sPageId = oApp.getPages()[1].getId();
+      oApp.to(sPageId);
+
+      var oPage = oApp.getPage(sPageId);
+      var oContext = oEvent.getSource().getBindingContext();
+      oPage.setBindingContext(oContext);
+    },
+  });
 });
