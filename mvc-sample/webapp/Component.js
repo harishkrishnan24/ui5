896b9726ccd941b6d30bbd0da903ab7de45b4701
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-18 15:31:31 +0530

    Add i18n and manifest

--- mvc-sample/webapp/Component.js
+++ mvc-sample/webapp/Component.js
@@ -5,51 +5,12 @@ sap.ui.define(
 
     return UIComponent.extend("sapui5.demo.mvcapp.Component", {
       metadata: {
-        rootView: "sapui5.demo.mvcapp.view.App",
-        config: {
-          serviceUrl: "webapp/service/data.json",
-        },
-        routing: {
-          config: {
-            routerClass: "sap.m.routing.Router",
-            viewType: "XML",
-            viewPath: "sapui5.demo.mvcapp.view",
-            controlId: "app",
-            controlAggregation: "pages",
-            transition: "slide",
-          },
-          routes: [
-            { pattern: "", name: "master", target: "master" },
-            { pattern: "detail/{ID}", name: "detail", target: "detail" },
-          ],
-          targets: {
-            master: {
-              viewName: "Master",
-              viewLevel: 1,
-            },
-            detail: {
-              viewName: "Detail",
-              viewLevel: 2,
-            },
-          },
-        },
+        manifest: "json",
       },
       init: function () {
         UIComponent.prototype.init.apply(this, arguments);
 
         this.getRouter().initialize();
-      },
-      createContent: function () {
-        var oRootView = UIComponent.prototype.createContent.apply(
-          this,
-          arguments
-        );
-
-        var oModel = new JSONModel(this.getMetadata().getConfig().serviceUrl);
-
-        this.setModel(oModel);
-
-        return oRootView;
       },
     });
   }
